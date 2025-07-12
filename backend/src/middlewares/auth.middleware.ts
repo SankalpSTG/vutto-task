6666149067a525dfcc01c4594cd2719bc1949436
Sankalp Pol <sankalppolk123@gmail.com>
2025-07-12 12:38:24 +0530

    Updated env

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -7,6 +7,7 @@ export const AuthMiddleware = () => {
     return async function(req: Request, res: Response, next: NextFunction) {
         const authToken = req.cookies?.["auth-token"]
         if(!authToken) throw new UnAuthorizedException()
+        console.log(process.env.ACCESS_TOKEN_SECRET)
         const response = JWTService.verify<JWTSignInPayload>(authToken, process.env.ACCESS_TOKEN_SECRET!)
         if(!response) throw new UnAuthorizedException()
         req.user = response
